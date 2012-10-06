39bdb042f081d084bdedbba9dc649cc36953ee3d
jiangyizhen3 <ethan3.jiang@gmail.com>
2012-10-06 20:18:17 +0800

    init the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Macropolo::Application.config.secret_token = '56c947c80db9b55aa47469830928d218eab938662a338328bef8db95fd912371f1b5c22bc302aa4c6286a7f97b05854071b1fb0e59dcb4823e8fed9c2c6c8eac'
