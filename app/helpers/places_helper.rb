e06eee9be2d5e6eae4fca4402791724cb78199b0
yizhen <yizhen@uvml.(none)>
2012-10-25 18:31:18 +0800

    introduce the gmap_image_tag

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,32 @@
 module PlacesHelper
+
+  def gmap_image_tag (lat, lng, types = nil)
+    size="600x400"
+    zoom = 4
+
+    if !types.nil?
+        pt = types[0].to_s
+        zoom = case pt
+          when "country"
+            3
+          when "administrative_area_level_1"
+            6
+          when "locality"
+            6
+          when "sublocality"
+            12
+          when "route", "neighborhood"
+            14
+          when "street_number", "street_address", "establishment", "point_of_interest"
+            16
+          else
+            print "\n**** unknown type for determing zoom:" + pt
+            8
+        end
+    end
+
+
+    tag = "<img src=\"http://maps.google.com/maps/api/staticmap?size=#{size}&sensor=false&zoom=#{zoom}&markers=#{lat}%2C#{lng}\">".html_safe
+  end
+
 end
